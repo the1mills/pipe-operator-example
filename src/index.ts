4b1331b77611838d489bdb8afbaea22f4967960d
Alexander Mills <t_milla@sfoc02xq5d0jg5j.ads.autodesk.com>
2019-04-23 17:16:32 -0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,34 @@
 'use strict';
 
-export const foo = 'bar';
 
 export const r2gSmokeTest = function () {
   // r2g command line app uses this exported function
   return true;
 };
 
-/*
 
- bad library design:
- module.exports = 'foo';
- export = 'foo';
+type SingleArgFunc<T = number, V = number> = (a: T) => V;
+type SAF = SingleArgFunc;
 
- good library design:
- exports.x = 'foo'
- export const x = 'foo'
- export default = 'foo';
+const _piper = (a: SAF, b: SAF) => (arg: any) => b(a(arg));
 
-*/
+const piper = (...funcs: Array<SAF>) => {
+
+  if (funcs.length === 1) {
+    funcs.push(v => v);
+  }
+
+  return (arg: any) => {
+    return funcs.reduce((a, b) => b(a), arg);
+  };
+
+};
 
 
+const _pipe = (a: SAF, b: SAF) => (arg: number) => b(a(arg));
+const pipe = (...ops: Array<SAF>): SAF => ops.reduce(_pipe);
+
+
+console.log(piper(v => 5 + v, v => v * 2)(3));
+console.log(pipe(v => 5 + v, v => v * 2)(3));
 
